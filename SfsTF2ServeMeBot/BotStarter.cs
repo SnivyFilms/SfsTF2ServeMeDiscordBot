008a8883289c8dd7e512471f132a99865a283d4d
Snivy Films <mill385@yahoo.com>
2025-09-22 13:15:07 -0400

    Version

--- SfsTF2ServeMeBot/BotStarter.cs
+++ SfsTF2ServeMeBot/BotStarter.cs
@@ -8,7 +8,7 @@ namespace SfsTF2ServeMeBot;
 
 public class BotStarter
 {
-    public static Version BotVersion = new Version(1, 2,2);
+    public static Version BotVersion = new Version(1, 3,0);
     public static async Task Main(string[] args)
     {
         var configuration = new ConfigurationBuilder()
